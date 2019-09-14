27f331b955a2b918946ef5357e01bcd5283e7059
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-14 15:46:02 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -4,7 +4,6 @@ describe '#square_array' do
 
   it 'does not call on each, collect, map, or inject' do
     numbers = [1,2,3]
-    expect(numbers).to_not receive(:each)
     expect(numbers).to_not receive(:collect)
     expect(numbers).to_not receive(:map)
     expect(numbers).to_not receive(:inject)
